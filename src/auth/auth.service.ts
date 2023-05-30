bc70f63ad43235c614d7c2c97e71d40e6ffb3570
Arif <arif520gg@gmail.com>
2023-05-30 21:26:42 +0400

    auth ~

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user-dto';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private authRepository: any){}
+    constructor(private userSevice: UsersService){} 
 
     async login(dto: CreateUserDto){
-        
+
     }
 
     async register(dto: CreateUserDto){
